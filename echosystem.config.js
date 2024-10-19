72981db0af6d7a0b2627010a6f4fa2a76734e005
arash <arash.golrokhian@hotmail.com>
2024-10-19 19:14:52 +0330

    feat: added echosystem

---
+++ echosystem.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  apps: [
+    {
+      name: "client",
+      script: "./node_modules/next/dist/bin/next",
+      args: "start -p " + (process.env.PORT || 3000),
+      watch: false,
+      autorestart: true,
+    },
+  ],
+};
