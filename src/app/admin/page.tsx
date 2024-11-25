332ff94b0c6fbb4ed93d2c3539656c0788d87139
arash <arash.golrokhian@hotmail.com>
2024-11-25 21:27:05 +0330

    feat: admin auth added

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import {
   Flex,
+  Spinner,
   Tab,
   TabList,
   TabPanel,
@@ -13,9 +14,23 @@ import { CheckTokens } from "@/components/admin-page/CheckTokens";
 import { CheckRequests } from "@/components/admin-page/CheckRequests";
 import { CheckMarketerRequests } from "@/components/admin-page/CheckMarketerRequests";
 import { SetMarketerOrMinter } from "@/components/admin-page/SetMarketerOrMinter";
+import { useActiveAccount, useReadContract } from "thirdweb/react";
+import { SportNFTContract } from "@/consts/nft_contracts";
+import { useRouter } from "next/navigation";
 
 export default function Page() {
   const isMobile = useMediaQuery("(max-width: 526px)");
+  const account = useActiveAccount();
+  const { data, isPending } = useReadContract({
+    contract: SportNFTContract,
+    method: "function owner() view returns (address)",
+    params: [],
+  });
+  const router = useRouter();
+  if (isPending) return <Spinner />;
+
+  if (data !== account?.address) router.push("/");
+
   return (
     <Flex
       justifyContent={"center"}
